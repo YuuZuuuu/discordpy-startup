b2d118fe8782cb72aa0ba7f31df5deb9c85eb941
YuuZuuuu <98651797+YuuZuuuu@users.noreply.github.com>
2022-01-29 23:39:50 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,7 +1,11 @@
 from discord.ext import commands
 from os import getenv
 import traceback
+import discord
+from discord.channel import VoiceChannel
 
+voiceChannel: VoiceChannel
+    
 bot = commands.Bot(command_prefix='/')
 
 
@@ -16,6 +20,28 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('pong')
 
+@client.event
+async def on_message(message):
+    global voiceChannel
+
+    if message.author.bot:
+        return
+    if message.content == '!connect':
+        voiceChannel = await VoiceChannel.connect(message.author.voice.channel)
+        await message.channel.send('ゆずよみが参加しました')
+    
+@client.event
+async def on_message(message):
+    global voiceChannel
+
+    if message.author.bot:
+        return
+   if message.content == '!disconnect':
+        voiceChannel.stop()
+        await message.channel.send('ゆずよみが退出しました')
+        await voiceChannel.disconnect()   
+    
+    
 
 token = getenv('DISCORD_BOT_TOKEN')
 bot.run(token)
